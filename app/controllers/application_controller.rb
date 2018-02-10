ed2994c685bde07f5061c6d96d520b1cec63ffef
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-10 13:07:57 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,21 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :index
+    end
+
+    post '/team' do
+
+      @team = Team.new(params[:team])
+
+      params[:member][:team].each do |m|
+        Member.new(m)
+      end
+
+      @members = Member.all
+
+      erd :teams
+    end
 
 end
